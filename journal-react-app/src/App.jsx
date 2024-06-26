8843fd7af4895ebe5781a811c6519cb47d7a76cc
bowendong1234 <bdon0009@student.monash.edu>
2024-06-26 22:00:57 +1000

    Added routing functionality between login page and editor page

--- journal-react-app/src/App.jsx
+++ journal-react-app/src/App.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
-import Editor from './Editor.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Login';
+import EditorPage from './Editor';
+import PrivateRoute from './PrivateRoute';
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <h1>React Editor.js pls work</h1>
-      <Editor />
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route 
+        path="/editor" 
+        element={
+          <PrivateRoute>
+            <EditorPage />
+          </PrivateRoute>
+        } 
+      />
+    </Routes>
   );
-};
+}
 
 export default App;
+// import React from 'react';
+// import Editor from './Editor.jsx';
+// import Login from './Login.jsx';
+// import ScrollContainer from './ScrollContainer.jsx';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+// const containerStyle = {
+//   backgroundColor: 'white',
+//   borderRadius: '10px',
+//   padding: '20px',
+//   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+//   height: '80vh',
+//   maxWidth: '300vh',
+//   margin: '10px',
+//   overflowY: 'auto',
+// };
+
+// const App = () => {
+
+//   return (
+//     <div style={containerStyle}>
+//       <div className="App">
+//         <ScrollContainer>
+//           <h2>hello</h2>
+//           <Editor />
+//         </ScrollContainer>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
