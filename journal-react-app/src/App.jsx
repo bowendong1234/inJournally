5496b20698029dbc313b74e50a1a37072d39d515
bowendong1234 <bdon0009@student.monash.edu>
2024-07-02 11:52:58 +1000

    Updated routing

--- journal-react-app/src/App.jsx
+++ journal-react-app/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './Login';
-import EditorPage from './Editor';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage'
+import EditorPage from './pages/EditorPage';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
       <Route 
         path="/editor" 
         element={
