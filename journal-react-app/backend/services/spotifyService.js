3915f676630d3c4189cbde7bac498f9aead16b1d
bowendong1234 <bdon0009@student.monash.edu>
2025-02-28 15:20:57 +1100

    Reduced number of streams fetched per user in each polling cycle

--- journal-react-app/backend/services/spotifyService.js
+++ journal-react-app/backend/services/spotifyService.js
@@ -77,7 +77,7 @@ async function pollSpotifyStreams() {
 }
 
 async function fetchSpotifyStreams(accessToken) {
-    const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=20', {
+    const response = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=6', {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
