aa1e0cc4eb6fc3fca7ad7e1241aca6f9393aea45
bowendong1234 <bdon0009@student.monash.edu>
2025-03-10 18:08:00 +1100

    Added disclaimer for account connection issue

--- journal-react-app/src/pages/LoginPage.jsx
+++ journal-react-app/src/pages/LoginPage.jsx
@@ -9,15 +9,22 @@ const LoginPage = () => {
     const handleSignUpClick = () => {
         navigate('/signup');
     };
-  
+
     const handleForgotPasswordClick = () => {
         navigate('/forgotpassword');
-      };
+    };
 
 
     return (
         <div class="background">
-            <Login onSignUpClick={handleSignUpClick} onForgotPasswordClick={handleForgotPasswordClick}/>
+            <div class="text-container">
+                <div class="heading">Welcome to inJournally</div>
+                <div class="subheading">Capture your day in words and discover its soundtrack by connecting your Spotify account!</div>
+                <div class="notice">Heads up! inJournally is currently awaiting extension approval from Spotify. If you'd like to use the
+                    music tracking feature, it may take up to 36 hours for your streaming data to start being collected.</div>
+
+            </div>
+            <Login onSignUpClick={handleSignUpClick} onForgotPasswordClick={handleForgotPasswordClick} />
         </div>
     )
 }
