80331043326698f13dd8a2d79b9cab94ca5d77ef
bowendong1234 <bdon0009@student.monash.edu>
2025-03-12 16:24:49 +1100

    Updated styling and messaging related to spotify account approval

--- journal-react-app/src/pages/LoginPage.jsx
+++ journal-react-app/src/pages/LoginPage.jsx
@@ -19,7 +19,7 @@ const LoginPage = () => {
         <div class="background">
             <div class="text-container">
                 <div class="heading">Welcome to inJournally</div>
-                <div class="subheading">Capture your day in words and discover its soundtrack by connecting your Spotify account!</div>
+                <div class="sub-heading">Capture your day in words and discover its soundtrack by connecting your Spotify account!</div>
                 <div class="notice">Heads up! inJournally is currently awaiting extension approval from Spotify. If you'd like to use the
                     music tracking feature, it may take up to 36 hours for your streaming data to start being collected.</div>
 
